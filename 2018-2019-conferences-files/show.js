71a1976366c85129b739087760c991aa332d966e
Jackie Tseng <jackietseng86@gmail.com>
2017-11-15 01:48:13 +0800

    add count down and highlight, finish v1

--- 2018-2019-conferences-files/show.js
+++ 2018-2019-conferences-files/show.js
@@ -4,9 +4,16 @@ window.onload=function(){
             td = tds[3]
         try {
             var targetDate = Date.parse(td.innerText)
-            if (targetDate - Date.now() < 1000*60*60*24*60) {
+            if (targetDate < Date.now()) {
+                td.style.color = 'gray'
+                td.style.textDecoration = 'line-through'
+            }
+            else if (targetDate - Date.now() < 1000*60*60*24*90) {
                 td.style.color = 'red'
                 td.style.fontWeight = 'bolder'
+                var delta = targetDate - Date.now()
+                var days = Math.floor(delta/(24*60*60*1000))
+                td.innerText = td.innerText + ' (' + days.toString() + ')'
             }
         }
         catch(e){}
